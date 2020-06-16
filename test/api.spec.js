001d704e3097e200f7431459dfda5d10a03d7aa9
Mariana Bassi da Silva <marianabassidasilva@Marianas-Air.fritz.box>
2020-06-16 17:44:15 +0200

    Including unit tests

---
+++ test/api.spec.js
@@ -0,0 +1,59 @@
+'use strict'
+
+const chai = require('chai'),
+  chaiHttp = require('chai-http'),
+  expect = chai.expect,
+  should = chai.should();
+
+chai.use(chaiHttp);
+
+const api = require('../api.js');
+
+describe('cache api', function() {
+  it('Should get a brand new value', async function() {
+    const res = await chai.request(api).get('/api/cache/buddy');
+    res.should.have.status(200);
+    res.body.should.be.an('object');
+    res.body.value.should.be.an('string')
+  });
+  it('Should get a list of keys', async function() {
+    const res = await chai.request(api).get('/api/cache');
+    res.should.have.status(200);
+    res.body.should.be.an('array').of.length(1);
+    res.body[0].should.be.equals('buddy')
+  });
+  it('Should update a key', async function() {
+    const res = await chai.request(api).get('/api/cache/buddy');
+    const oldValue = res.body.value;
+
+    await chai.request(api).put('/api/cache/buddy');
+    res.should.have.status(200);
+
+    const newRes = await chai.request(api).get('/api/cache/buddy');
+    newRes.should.have.status(200);
+    newRes.body.value.should.not.be.equals(oldValue);
+  });
+  it('Should delete a key', async function() {
+    const res = await chai.request(api).delete('/api/cache/buddy');
+    res.should.have.status(200);
+
+    const newRes = await chai.request(api).get('/api/cache');
+    newRes.should.have.status(200);
+    newRes.body.should.be.an('array').of.length(0);
+  });
+  it('Should delete all keys', async function() {
+    await chai.request(api).get('/api/cache/buddy1');
+    await chai.request(api).get('/api/cache/buddy2');
+    await chai.request(api).get('/api/cache/buddy3');
+    const listRes = await chai.request(api).get('/api/cache');
+    listRes.should.have.status(200);
+    listRes.body.should.be.an('array').of.length(3);
+
+    const res = await chai.request(api).delete('/api/cache');
+    res.should.have.status(200);
+
+    const newRes = await chai.request(api).get('/api/cache');
+    newRes.should.have.status(200);
+    newRes.body.should.be.an('array').of.length(0);
+  });
+});
